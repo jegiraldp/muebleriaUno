5d213a802318ceb8fef57af5c18208ce10756697
Daniel Giraldo <byrongc321@gmail.com>
2021-06-06 16:36:06 -0500

    cambios

--- routes/routes-empleados.js
+++ routes/routes-empleados.js
@@ -1,5 +1,5 @@
-const router = require('express').Router();
 const express = require('express');
+const router = express.Router();
 const empleados=require('../models/empleados');
 
 
@@ -16,24 +16,26 @@ router.get('/nuevoEmpleadoForm',async (req,res)=> {
     res.render('nuevoEmpleadoForm');
 });
 /////////
+
+
 //////////////////////////////
 router.post('/nuevoEmpleado',async (req,res)=> {
 const {documento,nombres,apellidos,correo}=req.body;
 if(documento.length==0   || nombres.length==0 || apellidos.length==0 || correo.length==0){
     //req.flash('error_registro',"Faltan datos del curso");
     res.redirect('/courses/nuevoEmpleadoForm');
-}else{
-  console.log(documento);
-  console.log(nombres);
-  console.log(apellidos);
-  console.log(correo);
-  //const nuevoEmpleado=new empleados({documento,nombres,apellidos,correo});
-  //await nuevoEmpleado.save();
-  //req.flash('ok_registro',"Curso registrado correctamente");
-  res.redirect('/courses/homeEmpleados');
+}
+else{
+console.log(documento);
+console.log(nombres);
+console.log(apellidos);
+console.log(correo);
+//const nuevoEmpleado=new empleados({documento,nombres,apellidos,correo});
+//await nuevoEmpleado.save();
+//req.flash('ok_registro',"Curso registrado correctamente");
+res.redirect('/courses/homeEmpleados');
 
-}//else
-
+}
 });
 
 ///////////////////////////
--- servidor.js
+++ servidor.js
@@ -4,12 +4,12 @@ const empleadosRoutes=require('./routes/routes-empleados');
 
 //init
 const app = express();
+
 //use
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'));
-app.use(express.static('public'));
-app.use('/main',mainRoutes);
+app.use('/',mainRoutes);
 app.use('/empleados',empleadosRoutes);
 
 //set
@@ -21,5 +21,5 @@ app.set('view engine','ejs');
 var port =process.env.PORT || 8080;
 
 const server = app.listen(port,()=>{
-    console.log("Servidor Mueblería 2021");
+    console.log("Servidor Mueblería 2001");
 });
